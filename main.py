a814e3d3d42ea079c593386343bf79c0b73da78d
ValeriaKravtsova <lera.gor@inbox.ru>
2020-06-09 20:19:22 +0300

    delete one file

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def elephant(max_try: int, fail_answer: str, win_answer: str, counter=0, arg_num
         print(my_arguments[arg_number])
         user_answer = input('Купи слона? ')
         if user_answer in yes_list:
-            return win_answer
+            print(win_answer)
         else:
             print('Решайся!')
         return elephant(max_try, fail_answer, win_answer, counter + 1, arg_number + 1)
 
     else:
-        return fail_answer
+        print(fail_answer)
 
 
 if __name__ == '__main__':
