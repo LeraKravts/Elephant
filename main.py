0b5b58ea903d7cd5fa798e6d27a84905c071cdd6
ValeriaKravtsova <lera.gor@inbox.ru>
2020-06-10 11:25:36 +0300

    исправлены небольшие баги

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from df import my_arguments, yes_list
 
 
-def elephant(max_try: int, fail_answer: str, win_answer: str, counter=0, arg_number=0) -> str:
+def elephant(
+        max_try: int,
+        fail_answer: str,
+        win_answer: str,
+        counter=0,
+):
     """Заставляю купить слона"""
     if counter <= max_try:
-        print(my_arguments[arg_number])
+        print(my_arguments[counter])
         user_answer = input('Купи слона? ')
         if user_answer in yes_list:
             print(win_answer)
         else:
-            print('Решайся!')
-        return elephant(max_try, fail_answer, win_answer, counter + 1, arg_number + 1)
+            elephant(
+                max_try,
+                fail_answer,
+                win_answer,
+                counter + 1,
+                )
 
     else:
         print(fail_answer)
